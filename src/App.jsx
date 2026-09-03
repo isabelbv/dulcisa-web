c599580941ac1aeee5eebfccf83d9a0f6136bf5f
CristhianQuiñonez <crisrobin-95@hotmail.com>
2026-09-03 21:25:28 +0200

    reestructurar proyecto a la raiz

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './Components/Navbar/Navbar';
+import Inicio from './pages/Inicio/Inicio';
+import Catalogo from './pages/Catalogo/Catalogo';
+import Footer from './Components/Footer/Footer';
+import Carrito from './pages/Carrito/Carrito';
+import Contacto from './pages/Contacto/Contacto';
+import { CarritoProvider } from './context/CarritoProvider';
+import './App.css';
+
+function App() {
+  return (
+    <CarritoProvider>
+      <Router>
+        <Navbar />
+        <main className='home-container'>
+          <Routes>
+            <Route path='/' element={<Inicio />} />
+            <Route path='/Home' element={<Inicio />} />
+            <Route path='/productos' element={<Catalogo />} />
+            <Route path='/carrito' element={<Carrito />} />
+            <Route path='/contacto' element={<Contacto />} />
+          </Routes>
+        </main>
+        <Footer />
+      </Router>
+    </CarritoProvider>
+  );
+}
+
+export default App;
